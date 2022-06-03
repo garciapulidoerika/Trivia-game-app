3ce210f98a6d30e514863ff6fc580f9b748263ca
garciapulidoerika <77468275+garciapulidoerika@users.noreply.github.com>
2022-06-03 16:02:30 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,75 +1,75 @@
 
-var position = 0;        //15   posicion 0 por defecto
-var dataQuestion = [];   //12
-var score = 0;           //27
-const container = document.getElementById("container-question");   //1
-const finishSection = document.getElementById("finish-section");   //2
+var position = 0;        
+var dataQuestion = [];   
+var score = 0;           
+const container = document.getElementById("container-question");   
+const finishSection = document.getElementById("finish-section");  
 
-function getQuestion() {                                                     //3 creamos aqui y ejecutamos en html
-  const totalQuestions = document.getElementById("n-questions").value;       //4
-  const categoryQuestions = document.getElementById("category").value;       //5
-  const difficultyQuestions = document.getElementById("difficulty").value;   //6
-  const typeQuestions = document.getElementById("type").value;               //7
-  const link = `https://opentdb.com/api.php?amount=${totalQuestions}&category=${categoryQuestions}&difficulty=${difficultyQuestions}&type=${typeQuestions}`;  //8
-  fetch(link)                               //9
-    .then((response) => response.json())    //10
-    .then((data) => {                       //11
-      dataQuestion = data;                  //13
+function getQuestion() {                                                     
+  const totalQuestions = document.getElementById("n-questions").value;       
+  const categoryQuestions = document.getElementById("category").value;       
+  const difficultyQuestions = document.getElementById("difficulty").value;  
+  const typeQuestions = document.getElementById("type").value;              
+  const link = `https://opentdb.com/api.php?amount=${totalQuestions}&category=${categoryQuestions}&difficulty=${difficultyQuestions}&type=${typeQuestions}`;  
+  fetch(link)                               
+    .then((response) => response.json())    
+    .then((data) => {                       
+      dataQuestion = data;                  
 
-      if (typeQuestions === "multiple") {                                      //19
-        container.innerHTML = renderQuestionsTemplate(dataQuestion.results);   //19
-      } else {                                                                 //19
-        container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    //19
+      if (typeQuestions === "multiple") {                                      
+        container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+      } else {                                                                 
+        container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    
       }
     });
 }
 
-function desorderPositionArray(array, iter) {               //20   argumento iter que se itera cierto numero de veces
-  let random = []                                           //21
+function desorderPositionArray(array, iter) {               
+  let random = []                                          
 
-  while (random.length < iter) {                            //21 analizar todo esto...
+  while (random.length < iter) {                            
 
-    let positionArray = Math.floor(Math.random() * iter)    //21
+    let positionArray = Math.floor(Math.random() * iter)    
 
-    if (!random.includes(array[positionArray])) {           //21
-      random.push(array[positionArray])                     //21
+    if (!random.includes(array[positionArray])) {           
+      random.push(array[positionArray])                     
     }
   }
-  return random                                             //21  ...y esperar
+  return random                                             
 }
 
 
-function hiddeForm() {                                      //ejecutar en html
+function hiddeForm() {                                     
   document.getElementById("form-cont").classList.add("dp-none");
 }
 
 
-function changeQuestion() {                                 //24
+function changeQuestion() {                                 
 
-  getInputsValue()                                          //33 fin
+  getInputsValue()                                          
 
-  if (position === dataQuestion.results.length - 1) {       //25 "si la posicion es igual a la posicion final"
-    container.style.display = "none"                              // se desaparece el display y sale esto..
+  if (position === dataQuestion.results.length - 1) {       
+    container.style.display = "none"                              
     finishSection.innerHTML = `<div class = "finish" style="width: 100%; padding: 100px 0px; text-align: center; font-size: 1.3rem;"><h1 class = "mb">Test finished<br> Score = ${score}</h1><a href="../pages/select.html">Play Again</a></div>`
     return
   }
 
-  position++;                                                             // averiguar que hace
-  const typeQuestions = document.getElementById("type").value;            // esto ya esta arriba en el 19
-  if (typeQuestions === "multiple") {                                     // esto ya esta arriba en el 19
-    container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  // esto ya esta arriba en el 19
-  } else {                                                                // esto ya esta arriba en el 19
-    container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   // esto ya esta arriba en el 19
+  position++;                                                             
+  const typeQuestions = document.getElementById("type").value;            
+  if (typeQuestions === "multiple") {                                     
+    container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+  } else {                                                                
+    container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   
   }
   return;
 
 }
 
 
-function renderQuestionsTemplate(question) {                 //14   argumento question
-  var array = question[position].incorrect_answers           //16   array: nombre que se le da al array creado / respuestas en la posicion 0 del arreglo
-  array.push(question[position].correct_answer)              //16
-  let newOrderQuestion = desorderPositionArray(array, 4)     //22  (array de linea 70, q se iterará en 4 ) / de aqui para abajo...
+function renderQuestionsTemplate(question) {                 
+  var array = question[position].incorrect_answers           
+  array.push(question[position].correct_answer)              
+  let newOrderQuestion = desorderPositionArray(array, 4)     
   return `<form class="form-test" onsubmit="event.preventDefault(), changeQuestion()">   
     <div class="question-form">
       <h2>${question[position].question}</h2>
@@ -103,10 +103,10 @@ function renderQuestionsTemplate(question) {                 //14   argumento qu
   </form>`;
 }
 
-function renderQuestionsBoolean(question) {                   //17  argumento question
-  var arraybool = question[position].incorrect_answers        //18  respuestas en la posicion 0 del arreglo
-  arraybool.push(question[position].correct_answer)           //18 
-  let newOrderQuestion = desorderPositionArray(arraybool, 2)  //23 de aqui para abajo...
+function renderQuestionsBoolean(question) {                   
+  var arraybool = question[position].incorrect_answers        
+  arraybool.push(question[position].correct_answer)           
+  let newOrderQuestion = desorderPositionArray(arraybool, 2)  
   return `<form class="form-test"  onsubmit="event.preventDefault(), changeQuestion()">
     <div class="question-form">
       <h2>${question[position].question}</h2>
@@ -130,14 +130,14 @@ function renderQuestionsBoolean(question) {                   //17  argumento qu
   </form>`;
 }
 
-// ahora vamos a sacar el resultado                           //26
-function getInputsValue() {                                   //28
-  let answerInput = document.getElementsByTagName("input");   //29 el input sale de los punticos
-  answerInput = Array.from(answerInput);                      //30
-  answerInput.map(element => {                                //31 que es lo que hace el map?
-    if (element.checked) {                                    //32
-      if (element.value === dataQuestion.results[position].correct_answer) {  //32
-        score = score + 10;                                   //32 arriba para ejecutar
+// ahora vamos a sacar el resultado                           
+function getInputsValue() {                                   
+  let answerInput = document.getElementsByTagName("input");   
+  answerInput = Array.from(answerInput);                     
+  answerInput.map(element => {                                
+    if (element.checked) {                                   
+      if (element.value === dataQuestion.results[position].correct_answer) {  
+        score = score + 10;                                  
       }
     }
   })
